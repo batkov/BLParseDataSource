48ff2a40c09d7a1b4a0ee0de3a83745ea270f9b9
Hariton Batkov <batkov@i.ua>
2016-04-18 14:41:46 +0300

    fix for offline

--- Pod/Classes/BLParseFetch.m
+++ Pod/Classes/BLParseFetch.m
@@ -92,13 +92,24 @@
     if (!self.offlineFetchAvailable) {
         return;
     }
-    NSMutableArray * items = [NSMutableArray array];
+    NSMutableArray * itemsToSave = [NSMutableArray array];
     for (NSArray * section in fetchResult.sections) {
-        [items addObjectsFromArray:section];
+        [itemsToSave addObjectsFromArray:section];
     }
     NSString * pinName = [self pinName];
-    [PFObject unpinAllObjectsInBackgroundWithName:pinName block:^(BOOL succeeded, NSError * error) {
-        [PFObject pinAllInBackground:items withName:pinName block:callback];
+    [self fetchOffline:^(id  _Nullable object, NSError * _Nullable error) {
+        if (error || ![object isKindOfClass:[NSArray class]]) {
+            [PFObject pinAllInBackground:itemsToSave withName:pinName block:callback];
+            return;
+        }
+        NSArray * itemsToRemove = (NSArray *) object;
+        [PFObject unpinAllInBackground:itemsToRemove
+                              withName:pinName
+                                 block:^(BOOL succeeded, NSError * _Nullable error) {
+            
+            [PFObject pinAllInBackground:itemsToSave withName:pinName block:callback];
+        }];
+        
     }];
 }
 
