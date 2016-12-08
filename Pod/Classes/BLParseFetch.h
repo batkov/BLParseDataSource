7bae39845e0ed14de45e56afceb2f0ca8186b201
Hariton Batkov <yanker90@gmail.com>
2016-12-08 10:42:24 +0200

    0.3.2

--- Pod/Classes/BLParseFetch.h
+++ Pod/Classes/BLParseFetch.h
@@ -22,7 +22,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-#import "BLBaseFetch.h"
+@import BLListDataSource;
 #import <Parse/Parse.h>
 
 typedef NSDictionary * (^BLParseCloudParamsBlock)(BLPaging * paging);
